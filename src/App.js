933adfe526fab9c17fc43b140fbd358032125b50
sahilvaidya13 <sahil.vaidya13@gmail.com>
2022-01-20 19:25:07 +0530

    center image added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ export default function App() {
   return (
     <div>
       <Navbar />
+      <div class="sp-img">
+        <h1 class="t1">Listening is</h1>
+        <h1 class="t2">everything</h1>
+        <h4 class="t3">
+          Millions of songs and podcasts. No credit card needed.
+        </h4>
+        <button class="main-btn">GET SPOTIFY FREE</button>
+      </div>
     </div>
   );
 }
